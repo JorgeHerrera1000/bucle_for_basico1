8ac066e9aadbad56c065d5e43a99caa3bc72a77b
JorgeHerrera1000 <jghm1000@gmail.com>
2024-08-06 18:18:42 -0400

    Primera Tarea completada

---
+++ bucle_for_basico1.py
@@ -0,0 +1,31 @@
+
+# for i in range(101):
+#     print(i)
+
+
+# for i in range(2, 501, 2):
+#     print(i)
+
+
+# for i in range(1,101):
+#     if i %10 == 0:
+#         print("baby")
+#     elif i %5 == 0:
+#         print("ice ice")
+#     else:
+#         print(i)
+
+# suma_total = sum(range(0,500001,2))
+# print(suma_total)
+
+# for resta_total in range(2024,0,-3):
+#     print(resta_total)
+
+
+# numInicial =3 
+# numFinal = 10 
+# multiplo = 2 
+
+# for i in range(numInicial,numFinal +1):
+#     if i% multiplo ==0:
+#         print(i)
